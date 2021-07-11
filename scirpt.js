e227447186a48bc83644f27e4cae1ce86a99d4f5
Sandeep <sandeepnandanwar92@gmail.com>
2021-07-11 17:34:27 +0530

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -2,7 +2,7 @@
 gapi.load("client", loadClient);
 
 function loadClient() {
-    gapi.client.setApiKey("AIzaSyBuVI2GP7j1GuP4WRgUDJ5K6iQpvyG4MH8");
+    gapi.client.setApiKey("AIzaSyCck5FU4j5_DMpslmk8GJ0VLVvECqVoq9s");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function () { console.log("GAPI client loaded for API"); },
             function (err) { console.error("Error loading GAPI client for API", err); });
